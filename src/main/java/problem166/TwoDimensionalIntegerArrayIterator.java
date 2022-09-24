71d1d4ab40b2c919d9ac3b67e6de102ee57fe7f6
Sean Voeller <sean.voeller@gmail.com>
2022-09-24 14:23:26 -0700

    initial commit

---
+++ src/main/java/problem166/TwoDimensionalIntegerArrayIterator.java
@@ -0,0 +1,15 @@
+package problem166;
+
+import java.util.Iterator;
+
+public class TwoDimensionalIntegerArrayIterator implements Iterator<Integer> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        return null;
+    }
+}
