bd0f068fdaa50472b5b0c9be536acd6e908f1d09
Sean Voeller <sean.voeller@gmail.com>
2022-09-24 15:07:46 -0700

    problem #166 - initial solution

--- src/main/java/problem166/TwoDimensionalIntegerArrayIterator.java
+++ src/main/java/problem166/TwoDimensionalIntegerArrayIterator.java
@@ -1,15 +1,60 @@
 package problem166;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class TwoDimensionalIntegerArrayIterator implements Iterator<Integer> {
+    private final int[][] outerArray;
+    private int x = 0;
+    private int y = 0;
+    private Integer next = null;
+
+    public TwoDimensionalIntegerArrayIterator(int[][] input) {
+        this.outerArray = input;
+    }
+
     @Override
     public boolean hasNext() {
-        return false;
+        if (next != null) {
+            return true;
+        }
+        next = findNext();
+        return next != null;
     }
 
     @Override
     public Integer next() {
-        return null;
+        if (next != null) {
+            int tmp = next;
+            next = null;
+            return tmp;
+        }
+        Integer theNext = findNext();
+        if (theNext == null) {
+            throw new NoSuchElementException("No next element");
+        }
+        return theNext;
+    }
+
+    private Integer findNext() {
+        // our current X position is outside the bounds of the outer array, return null indicating that there is no next
+        // element
+        if (x >= outerArray.length) {
+            return null;
+        }
+
+        // fetch inner array at current X position and advance X position by one.
+        int[] innerArray = outerArray[x];
+
+        // advance to the next inner array if the current one is empty
+        while (y >= innerArray.length) {
+            if (++x >= outerArray.length) {
+                return null;
+            }
+            innerArray = outerArray[x];
+            y = 0;
+        }
+
+        return innerArray[y++];
     }
 }
--- src/test/java/problem166/TwoDimensionalIntegerArrayIteratorTest.java
+++ src/test/java/problem166/TwoDimensionalIntegerArrayIteratorTest.java
@@ -1,6 +1,9 @@
 package problem166;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +19,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class TwoDimensionalIntegerArrayIteratorTest {
     @Test
     public void testTwoDimIterator() {
+        int[][] input = new int[][]{
+                {1, 2},
+                {3},
+                {},
+                {4, 5, 6}
+        };
 
+        Iterator<Integer> iterator = new TwoDimensionalIntegerArrayIterator(input);
+
+        List<Integer> iterationResult = new ArrayList<>();
+        while(iterator.hasNext()) {
+            iterationResult.add(iterator.next());
+        }
+
+        List<Integer> expectedResult = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Assertions.assertEquals(expectedResult, iterationResult);
+    }
+
+    @Test
+    public void testTwoDimIteratorThrowsIfNextIsCalledWithNoRemainingElems() {
+        Assertions.assertThrows(
+            NoSuchElementException.class,
+            () -> {
+                Iterator<Integer> iterator = new TwoDimensionalIntegerArrayIterator(new int[][]{});
+                iterator.next();
+            }
+        );
     }
 }
